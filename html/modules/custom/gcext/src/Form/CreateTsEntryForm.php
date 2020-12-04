b4675dfa85e37fa162600e9308e5a2329851f004
Joel Brockbank <joel_osc@124931.no-reply.drupal.org>
2020-12-04 17:25:34 -0500

    Adding timesheet blocks

---
+++ html/modules/custom/gcext/src/Form/CreateTsEntryForm.php
@@ -0,0 +1,93 @@
+<?php
+namespace Drupal\gcext\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class CreateTsEntryForm.
+ *
+ * @package Drupal\gcext\Form
+ */
+class CreateTsEntryForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'create_tsentry_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $args = array()) {
+
+    //$user = \Drupal\user\Entity\User::load($uid);
+  
+    $node = $args['node'];
+
+    $form['nid'] = array(
+      '#type' => 'hidden',
+      '#value' => $node->id(),
+    );
+
+    // @TODO add team?
+
+    $form['add_tsentry'] = array(
+      '#type' => 'details',
+      '#title' => t('Log time'),
+      //'#open' => TRUE,
+    );
+
+    $form['add_tsentry']['summary'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Activity summary'),
+      '#rows' => 3,
+      '#required' => TRUE,
+    ];
+
+    $form['add_tsentry']['hours'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Hours'),
+      '#size' => 5,
+      '#maxlength' => 5,
+      '#number_type' => 'decimal',
+      '#field_name' => 'add_tsentry',
+      '#element_validate' => array ('number_field_widget_validate'),
+      '#input' => TRUE,
+      '#required' => TRUE,
+    ];
+  
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $nid = $form_state->getValue('nid');
+    $uid = \Drupal::currentUser()->id();
+
+    $title = substr($form_state->getValue('summary'), 0, 50);
+    $node = \Drupal::entityTypeManager()->getStorage('node')->create(['type' => 'entry']);
+    $node->set('title', $title);
+    $node->set('field_ts_summary', $form_state->getValue('summary'));
+    $node->set('field_ts_hours', $form_state->getValue('hours'));
+    $node->set('field_ts_case', $nid);
+    $node->status = 1;
+    $node->uid = $uid;
+    $node->enforceIsNew();
+    $node->save();
+  
+    \Drupal::messenger()->addMessage($this->t("Time entry was successfully created."));
+  }
+
+}
---
+++ html/modules/custom/gcext/src/Plugin/Block/CreateTsEntryBlock.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\gcext\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a add timesheet entry Block.
+ *
+ * @Block(
+ *   id = "create_tsentry_block",
+ *   admin_label = @Translation("Create timesheet entry block"),
+ *   category = @Translation("GCExt"),
+ *   context = {
+ *     "node" = @ContextDefinition("entity:node")
+ *   }
+ * )
+ */
+class CreateTsEntryBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $node = $this->getContextValue('node');
+    $form = \Drupal::formBuilder()->getForm('\Drupal\gcext\Form\CreateTsEntryForm', array('node' => $node));
+
+    return $form;
+  }
+
+}
