802d12951dc7c140d9331c53fff1019c3ad50fe9
EC2 Default User <ec2-user@ip-172-31-17-80.ca-central-1.compute.internal>
2020-12-06 12:15:57 +0000

    Adding tab for time log

---
+++ html/modules/custom/gcext/src/Plugin/Derivative/DynamicLocalTasks.php
@@ -0,0 +1,69 @@
+<?php
+  
+namespace Drupal\gcext\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\StringTranslation\TranslationManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines dynamic local tasks.
+ */
+class DynamicLocalTasks extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The translation manager.
+   *
+   * @var \Drupal\Core\StringTranslation\TranslationManager
+   */
+  protected $translationManager;
+
+  /**
+   * Constructs a new DynamicLocalTasks.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationManager $string_translation
+   *   The translation manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, TranslationManager $string_translation) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->translationManager = $string_translation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('string_translation')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+
+    $this->derivatives = [];
+    $this->derivatives["node.time_tab"] = [
+      'route_name' => "page_manager.page_view_time_log_time_log-panels_variant-0",
+      'title' => $this->translationManager->translate('Time log'),
+      'base_route' => "entity.node.canonical",
+      'weight' => 2,
+    ];
+
+    return $this->derivatives;
+  }
+
+}
