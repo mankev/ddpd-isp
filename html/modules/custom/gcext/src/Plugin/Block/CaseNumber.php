2f7da40eae1ec491502bb859c628d9e8e763d327
EC2 Default User <ec2-user@ip-172-31-17-80.ca-central-1.compute.internal>
2021-01-06 03:15:58 +0000

    added case number block

---
+++ html/modules/custom/gcext/src/Plugin/Block/CaseNumber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\gcext\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Provides the case number (NID) in a block.
+ *
+ * @Block(
+ *   id = "op_case_number",
+ *   admin_label = @Translation("Case number"),
+ *   category = @Translation("Gcext"),
+ *   context = {
+ *     "node" = @ContextDefinition("entity:node")
+ *   }
+ * )
+ */
+class CaseNumber extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    $build = [];
+
+    // Node context.
+    if (is_object($node)) {
+
+      $caseNumber = $node->id();
+      if ($caseNumber) {
+        $build['case_number']['#markup'] = '<p>' . $caseNumber . '</p>';
+      }
+    }
+
+    return $build;
+  }
+
+}
